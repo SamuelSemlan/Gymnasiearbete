5a81b283d30bbbc81db3821851efc4a5c4364ccf
SamuelSemlan <semlan12@live.com>
2018-01-20 23:15:25 +0100

    Added grenade tossing and Post-Processing

---
+++ FPS/Assets/Scripts/Grenade.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour {
+
+	public float delay = 3f;
+	public float radius = 5f;
+	public float force = 700;
+
+	public GameObject explosionEffect;
+
+	float countDown;
+	bool hasExploded = false;
+
+	void Start () {
+		countDown = delay;
+	}
+	
+	void Update () {
+		countDown -= Time.deltaTime;
+		if(countDown <= 0f && !hasExploded){
+			Explode();
+			hasExploded = true;
+		}
+	}
+
+	void Explode(){
+		GameObject effect = Instantiate(explosionEffect, transform.position, transform.rotation);
+
+		Collider[] collidersToDestroy = Physics.OverlapSphere(transform.position, radius);
+
+		foreach(Collider nearbyObject in collidersToDestroy){
+
+			Destructible dest = nearbyObject.GetComponent<Destructible>();
+			if(dest != null){
+				dest.Destroy();
+			}
+		}
+
+		Collider[] collidersToShatter = Physics.OverlapSphere(transform.position, radius);
+
+		foreach(Collider nearbyObject in collidersToShatter){
+			Rigidbody rb = nearbyObject.GetComponent<Rigidbody>();
+			if(rb != null){
+				rb.AddExplosionForce(force, transform.position, radius);
+			}
+		}
+		Destroy(gameObject);
+		Destroy(effect.gameObject, 2.5f);
+	}
+}
---
+++ FPS/Assets/Scripts/GrenadeThrow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrow : MonoBehaviour {
+
+	public float throwForce = 300f;
+	public GameObject grenadePrefab;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.G)){
+			ThrowGrenade();
+		}
+	}
+
+
+	void ThrowGrenade(){
+		GameObject grenade = Instantiate(grenadePrefab, transform.position, transform.rotation);
+		Rigidbody rb = grenade.GetComponent<Rigidbody>();
+		rb.AddForce(transform.forward * throwForce * Time.deltaTime, ForceMode.VelocityChange);
+	}
+}
