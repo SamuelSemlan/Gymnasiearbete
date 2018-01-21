527b4d76eb53951cf27464e2c7f49c540ba5ae78
SamuelSemlan <semlan12@live.com>
2018-01-21 18:17:37 +0100

    Added Terrain

--- FPS/Assets/Scripts/Destructible.cs
+++ FPS/Assets/Scripts/Destructible.cs
@@ -6,13 +6,12 @@ public class Destructible : MonoBehaviour {
 
 	public GameObject destroyedVersion;
 
-	// If the player clicks on the object
 	public void Destroy()
 	{
 		// Spawn a shattered object
 		Instantiate(destroyedVersion, transform.position, transform.rotation);
 		// Remove the current object
-		//Destroy(gameObject);
+		Destroy(gameObject);
 	}
 
 }
